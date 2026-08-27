98da33bfcb08c8d56f248a6b4e8efe3c988ad1b8
willscoffee95-collab <willscoffee95@gmail.com>
2026-08-27 23:42:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ window.WILLS_CONFIG = Object.freeze({
   VERSION: '0.2.0-live-readonly',
   GITHUB_ORIGIN: 'https://willscoffee95-collab.github.io',
   // Paste URL deployment Web App Warehouse yang berakhiran /exec.
-  APPS_SCRIPT_URL: 'PASTE_APPS_SCRIPT_WEB_APP_URL_HERE'
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwlVWmt6QCBMj9Rr80qCzNFYNBGvqhqitHKizG8RBJS7lVNJDTy9NMtN67S6v-SoyMnoQ/exec'
 });
