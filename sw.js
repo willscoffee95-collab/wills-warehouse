c21ad9a6d5da12e0e222549e005a0ae5d9690656
willscoffee95-collab <willscoffee95@gmail.com>
2026-09-03 08:34:22 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'wills-warehouse-shell-v0.6.0-finance-stock-report';
+const CACHE = 'wills-warehouse-shell-v0.6.1-simple-finance-position';
 const ASSETS = [
-  './','./index.html','./app.css?v=0.6.0','./config.js?v=0.6.0','./bridge.js?v=0.6.0','./app.js?v=0.6.0','./manifest.webmanifest',
+  './','./index.html','./app.css?v=0.6.1','./config.js?v=0.6.1','./bridge.js?v=0.6.1','./app.js?v=0.6.1','./manifest.webmanifest',
   './logo.png','./icon-192.png','./icon-512.png','./maskable-512.png','./apple-touch-icon.png','./favicon-64.png'
 ];
 
